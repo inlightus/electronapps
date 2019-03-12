3ed19d7934842da0ba7e4a27bdcf47c273228d85
inlightus <inlight.ur.life@gmail.com>
2019-03-13 03:19:45 +0900

    voc

---
+++ voc/src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'voc',
+  };
+</script>
+
+<style>
+  /* CSS */
+</style>
